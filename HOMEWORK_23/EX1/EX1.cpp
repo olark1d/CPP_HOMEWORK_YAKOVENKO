50c0298031639695776e293425d2589d7ba6f1a8
Oleg <olark1d@gmail.com>
2024-08-03 23:03:27 +0300

    Adding HW23

---
+++ HOMEWORK_23/EX1/EX1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <sstream>
+#include <algorithm>
+
+unsigned int uniqueWordsCount(const std::string& line) 
+{
+    std::set<std::string> uniqueWords;
+    std::istringstream stream(line);
+    std::string word;
+
+    while (stream >> word) 
+    {
+            if (!uniqueWords.empty())
+            {
+                auto finded = uniqueWords.find(word);
+                if (finded != uniqueWords.end())
+                {
+                    uniqueWords.erase(word);
+                }
+                else
+                {
+                    uniqueWords.insert(word);
+                };
+            }
+            else
+            {
+                uniqueWords.insert(word);
+            };
+    }
+
+    return uniqueWords.size();
+}
+
+int main()
+{
+    std::string line;
+    std::cout << "Enter your words: ";
+    std::getline(std::cin, line);
+
+    std::cout << "Number of unique words = " << uniqueWordsCount(line) << std::endl;
+}
---
+++ HOMEWORK_23/EX3/EX3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+bool areBracketsBalanced(const std::string& line) 
+{
+    std::stack<char> brackets;
+
+    for (char ch : line) {
+        if (ch == '(' || ch == '{' || ch == '[') 
+        {
+            brackets.push(ch);
+        }
+        else if (ch == ')' || ch == '}' || ch == ']') 
+        {
+            if (brackets.empty()) return false;
+
+            char top = brackets.top();
+            brackets.pop();
+
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '[')) 
+                {
+                return false;
+                }
+        }
+    }
+
+    return brackets.empty();
+}
+
+int main()
+{
+    std::string line;
+    std::cout << "Enter your brackets: ";
+    std::getline(std::cin, line);
+
+    if (areBracketsBalanced(line))
+        std::cout << "OK\n";
+    else
+        std::cout << "NOT OK\n";
+}
